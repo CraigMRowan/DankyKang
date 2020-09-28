58ec3f7f0661241357322659750b8600945a0632
Craig Rowan <craigmrowan@gmail.com>
2020-09-28 21:26:05 +1300

    Added barrel spawner

---
+++ Assets/Scripts/BarrelSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelSpawner : MonoBehaviour
+{
+    public GameObject barrel;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+    public float barrelForce;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnBarrel", spawnTime, spawnDelay);
+    }
+
+    void SpawnBarrel()
+    {
+        GameObject cloneBarrel = (GameObject)Instantiate(barrel, transform.position, transform.rotation);
+        Rigidbody2D rigidbody2D = cloneBarrel.GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = Vector3.right * barrelForce;
+
+        if (stopSpawning)
+            CancelInvoke("SpawnBarrel");
+    }
+}
