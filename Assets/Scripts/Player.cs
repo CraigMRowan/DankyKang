a390ef799f3cd1f3fb358510369604a146fbf71a
Craig Rowan <craigmrowan@gmail.com>
2020-09-30 11:22:30 +1300

    Added barrel collision system with 3 lives

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 0f;
     [SerializeField] private float jumpForce = 0f;
     [SerializeField] private LayerMask floorLayerMask = default;
+    [SerializeField] private GameObject livesPanel = null;
 
     private Rigidbody2D _rigidBody;
     private SpriteRenderer _spriteRenderer;
     private Animator _myAnimator;
+    private int _currentLives = 3;
+    private Image[] _lifeImages = new Image[3];
 
     private static readonly int IsMoving = Animator.StringToHash("IsMoving");
 
@@ -16,8 +21,8 @@ public class Player : MonoBehaviour
     {
         _rigidBody = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-
         _myAnimator = GetComponent<Animator>();
+        _lifeImages = livesPanel.GetComponentsInChildren<Image>();
     }
 
     private void FixedUpdate()
@@ -31,7 +36,6 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.UpArrow))
         if (Input.GetButtonDown("Jump") && IsGrounded())
             _rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
 
@@ -45,5 +49,34 @@ public class Player : MonoBehaviour
     {
         var hit = Physics2D.Raycast(transform.position + new Vector3(0, -0.2f, 0), Vector2.down, 0.33f, floorLayerMask);
         return hit.collider != null;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Barrel"))
+        {
+            if (_currentLives > 1 )
+            {
+                RemoveLifeImage();
+                _currentLives--;
+                
+                //TODO: Move Player back to start
+                //TODO: Destroy Barrels?
+            }
+            else
+            {
+                RemoveLifeImage();
+                _currentLives--;
+
+                SceneManager.LoadScene("Level1"); //Temporary
+                //TODO: Game over
+                //TODO: Return to main menu
+            }
+        }
+    }
+
+    public void RemoveLifeImage()
+    {
+        _lifeImages[_currentLives - 1].gameObject.SetActive(false);
     }
 }
