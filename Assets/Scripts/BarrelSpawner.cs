2ea56013915426945e4fe171f55a44b4822cfcaa
Craig Rowan <craigmrowan@gmail.com>
2020-09-30 09:41:27 +1300

    Added BarrelSpawner parent object

--- Assets/Scripts/BarrelSpawner.cs
+++ Assets/Scripts/BarrelSpawner.cs
@@ -8,17 +8,19 @@ public class BarrelSpawner : MonoBehaviour
     [SerializeField] private float barrelForce = 0f;
 
     private bool _stopSpawning = false;
+    private GameObject _barrelParent = null;
 
     private void Start()
     {
         InvokeRepeating(nameof(SpawnBarrel), spawnTime, spawnDelay);
+        _barrelParent = new GameObject("Barrels");
     }
 
     private void SpawnBarrel()
     {
         var transform1 = transform;
 
-        var cloneBarrel = Instantiate(barrel, transform1.position, transform1.rotation);
+        var cloneBarrel = Instantiate(barrel, transform1.position, transform1.rotation, _barrelParent.transform);
         var localRigidbody2D = cloneBarrel.GetComponent<Rigidbody2D>();
         localRigidbody2D.velocity = Vector3.right * barrelForce;
 
